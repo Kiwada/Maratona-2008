17b84142851e665a0cd4137ade1a0141813bbef6
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-07-05 14:11:30 -0300

    clube.py

--- O Salão Do Clube/clube.py
+++ O Salão Do Clube/clube.py
@@ -1,51 +1,49 @@
 '''
-M = Largura
-N = Comprimento
-
-L = Largura da Tabua
-
-K = número de tabuas doadas = [] lista que vai variar de acordo com a quantidade de tabuas
-
+Bacharelado em ciencia da computação Bloco 2 
+05/07/2023
+Kaio Felipe Castro Fontenele 
+Arthur Dutra Costa Lima 
 
 '''
 
 
 arquivo = open("valores.txt","r")
-lista = []
+lista = [] 
+quant = 0  
+
+
 for i in arquivo:
-    
-    i = i.strip()
-    
-    lista.append(i)
+    i = i.strip() 
+    quant+=1 
+    lista.append(i) 
 
 
-lista[0] = [float(valor) for valor in lista[0].split()]
-lista[1] = [float(valor) for valor in lista[1].split()]
+for i in range(0, quant,2):
+    if lista[i]== '0':
+        break
+    lista[i] = [float(valor) for valor in lista[i].split()]
+    lista[i+1] = [float(valor) for valor in lista[i+1].split()]
+    dados = lista[i]
+    comprimentos = lista[i+1]
+    comprimentos = sorted(comprimentos) 
+    A = dados[0]*dados[1]
+    K = dados[-1]  
+    L = dados[2]   
+    L /=100
+    soma = 0
+    quantidade = 0
+    i=1
 
-dados = lista[0]
-
-comprimentos = lista[1]
-
-comprimentos = sorted(comprimentos)
 
 
-A = dados[0]*dados[1]
-K = dados[-1]
-L = dados[2]
-L /=100
-
-soma = 0
-quantidade = 0
-i=1
-
-while i<=K:
-    soma += comprimentos[-i] * L
-    quantidade+=1
-    if soma > A:
-        soma-=comprimentos[-i] * L
-        quantidade-=1
-    i+=1
-if soma == A:
-    print(quantidade)
-else:
-    print("impossivel")
+    while i<=K:
+        soma += comprimentos[-i] * L
+        quantidade+=1
+        if soma > A:
+            soma-=comprimentos[-i] * L
+            quantidade-=1
+        i+=1
+    if soma == A:
+        print(quantidade)
+    else:
+        print("impossivel")
