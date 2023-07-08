dad11e327fc127752affcf664c4dae861c7e41c6
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-07-07 21:02:33 -0300

    Update clube.py

--- O Salão Do Clube/clube.py
+++ O Salão Do Clube/clube.py
@@ -8,42 +8,48 @@ Arthur Dutra Costa Lima
 
 
 arquivo = open("valores.txt","r")
-lista = [] 
-quant = 0  
-
-
+lista = []
+quant = 0
 for i in arquivo:
-    i = i.strip() 
-    quant+=1 
-    lista.append(i) 
-
+    i = i.strip()
+    quant+=1
+    lista.append(i)
 
 for i in range(0, quant,2):
-    if lista[i]== '0':
+    if lista[i] == '0 0 0 0':
+        print("---")
         break
     lista[i] = [float(valor) for valor in lista[i].split()]
     lista[i+1] = [float(valor) for valor in lista[i+1].split()]
+    
     dados = lista[i]
     comprimentos = lista[i+1]
-    comprimentos = sorted(comprimentos) 
+    comprimentos = sorted(comprimentos)
     A = dados[0]*dados[1]
-    K = dados[-1]  
-    L = dados[2]   
+    K = dados[-1]
+    L = dados[2]
     L /=100
-    soma = 0
     quantidade = 0
-    i=1
-
-
-
+    
+    lista_madeiras = list(set(comprimentos))
+    soma_simples = sum(lista_madeiras)
+    
+    for i in lista_madeiras:
+        if i in comprimentos:
+            comprimentos.remove(i)
+            quantidade+=1
+            K-=1
+    
+    i = 0       
     while i<=K:
-        soma += comprimentos[-i] * L
+        soma_simples += comprimentos[-i] * L
         quantidade+=1
-        if soma > A:
-            soma-=comprimentos[-i] * L
+        if soma_simples > A:
+            soma_simples-=comprimentos[-i] * L
             quantidade-=1
         i+=1
-    if soma == A:
+    
+    if soma_simples == A:
         print(quantidade)
     else:
         print("impossivel")
